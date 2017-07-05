32fb14c59884e88a5989040d3b9ba08ba00203ce
Jami Cope <jamicope@gmail.com>
2017-07-05 11:41:19 -0600

    adding recipe, syntax corrections on app.post

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList, Recipes} = require('./models');
+const {
+    ShoppingList,
+    Recipes
+} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -21,37 +23,59 @@ ShoppingList.create('peppers', 4);
 // adding some recipes to `Recipes` so there's something
 // to retrieve.
 Recipes.create(
-  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
+    'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
 Recipes.create(
-  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+    'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+Recipes.create(
+    'scrambled eggs', ['eggs', 'milk', 'salt', 'pepper']);
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
-  res.json(ShoppingList.get());
+    res.json(ShoppingList.get());
 });
 
 app.post('/shopping-list', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'budget'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
+    // ensure `name` and `budget` are in request body
+    const requiredFields = ['name', 'budget'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
     }
-  }
 
-  const item = ShoppingList.create(req.body.name, req.body.budget);
-  res.status(201).json(item);
+    const item = ShoppingList.create(req.body.name, req.body.budget);
+    res.status(201).json(item);
 });
 
+app.post('/recipes', jsonParser, (req, res) => {
+    //there are 2 required fields the user must input items into
+    const requiredFields = ['name', 'ingredients'];
+    //set up a for loop to look for required fields. If fields are empty, return an error message
+    for (let i = 0; i < requiredFields.length; i++) {
+        //set up the var for what the loop is looking for- 'field' includes both requiredFields
+        //and it has to have input from user [i]
+        const field = requiredFields[i];
+        //if requiredFields are empty, show an error
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+
+    //if fields are filled by user appropriately, create anew recipe
+    const recipe = Recipe.create(req.body.name, req.body.ingredients);
+    res.status(201).json(item);
+});
 
 app.get('/recipes', (req, res) => {
-  res.json(Recipes.get());
+    res.json(Recipes.get());
 })
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
